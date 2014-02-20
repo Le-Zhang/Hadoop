5c5f0f65f53e02a5702839d3928e1db14473b064
Le Zhang <lezhang@live.unc.edu>
2014-02-20 00:39:13 -0500

    Compute the strength between studies and output the top pairs
    Compute strength based on author

---
+++ final project/ComputeByAuth_q3/Auth.java
@@ -0,0 +1,44 @@
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+
+public class Auth {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		if(args.length != 2) {
+			System.err.println("Usage: MaxTemperature <input path> <output path>");
+		    System.exit(-1);
+		}
+		
+		JobConf conf = new JobConf(Auth.class);
+		conf.setJobName("Count outgoing link per document");
+		
+		FileInputFormat.addInputPath(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		
+		
+		conf.setMapperClass(AuthMapper.class);
+		conf.setReducerClass(AuthReducer.class);
+		conf.setNumReduceTasks(50);
+		
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(Text.class);
+		
+		JobClient.runJob(conf);
+
+	}
+
+}
